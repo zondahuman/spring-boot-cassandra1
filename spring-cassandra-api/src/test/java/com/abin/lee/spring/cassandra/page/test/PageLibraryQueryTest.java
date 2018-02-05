fd77e546e173e7cb932c87b3fbcbcf807faa3022
zondahuman <zondahuman@gmail.com>
2018-02-05 17:21:01 +0800

    add

---
+++ spring-cassandra-api/src/test/java/com/abin/lee/spring/cassandra/page/test/PageLibraryQueryTest.java
@@ -0,0 +1,12 @@
+package com.abin.lee.spring.cassandra.page.test;
+
+/**
+ * Created by abin on 2018/2/5 17:06.
+ * spring-boot-cassandra1
+ * com.abin.lee.spring.cassandra.page.test
+ */
+public class PageLibraryQueryTest {
+
+
+
+}
